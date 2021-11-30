29a2c9753fe5155b8866c526289def02c138c45e
cdROma <mrv24ru@yandex.ru>
2021-12-01 00:11:54 +0300

    Update aws.js

--- src/utils/aws.js
+++ src/utils/aws.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import aws from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3.js';
 import * as FileType from 'file-type';
 const {DEFAULT_DIR, SPACE_NAME, SPACE_ENDPOINT} = process.env;
 
 // Set S3 endpoint to DigitalOcean Spaces
 const spacesEndpoint = new aws.Endpoint(SPACE_ENDPOINT);
-const s3 = new aws.S3({
+const s3 = new S3({
 	endpoint: spacesEndpoint,
 });
 
